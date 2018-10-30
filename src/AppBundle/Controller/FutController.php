3afa6863e46e14f056f3cbef383dcb7d70a65265
poulain35 <poulain.jeremy1@gmail.com>
2018-10-30 21:49:08 +0100

    Init FUT

--- src/AppBundle/Controller/FutController.php
+++ src/AppBundle/Controller/FutController.php
@@ -2,8 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use FUTApi\Core;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Psr\Log\LoggerInterface;
 
 
 class FutController extends Controller
@@ -13,7 +15,36 @@ class FutController extends Controller
      */
     public function indexAction()
     {
-        return new Response("Bienvenue sur FUT ");
+
+        $content = $this->container->get('templating')->render('AppBundle:Fut:index.html.twig');
+
+        return new Response($content);
+    }
+
+    /**
+     * Create a new connexion.
+     *
+     */
+    public function connectAction()
+    {
+        $loLogger = $this->get('logger');
+
+        try {
+            $fut = new Core('airness35@hotmail.com', 'Sinsemilia1', 'ps4', '006098');
+        } catch(FutError $e) {
+            $error = $e->GetOptions();
+            die("We have an error logging in: ".$error['reason']);
+        }
+
+        $loLogger->error(var_dump($fut));
+
+        $login = $fut->login();
+
+        $fut->logout();
+
+        return $login;
+        //$items = $fut->searchAuctions('player');
+
     }
 
 }

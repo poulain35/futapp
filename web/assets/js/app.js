9aaea9d610b03ec8517e56eece79b36522221081
poulain35 <poulain.jeremy1@gmail.com>
2018-11-11 20:59:41 +0100

    webpack

---
+++ web/assets/js/app.js
@@ -0,0 +1,5 @@
+// public/assets/js/app.js
+
+require('../css/app.css');
+
+console.log('Hello Webpack Encore');
---
+++ web/build/app.js
@@ -0,0 +1,29 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["app"],{
+
+/***/ "./web/assets/css/app.css":
+/*!********************************!*\
+  !*** ./web/assets/css/app.css ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./web/assets/js/app.js":
+/*!******************************!*\
+  !*** ./web/assets/js/app.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// public/assets/js/app.js
+__webpack_require__(/*! ../css/app.css */ "./web/assets/css/app.css");
+
+console.log('Hello Webpack Encore');
+
+/***/ })
+
+},[["./web/assets/js/app.js","runtime"]]]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi93ZWIvYXNzZXRzL2Nzcy9hcHAuY3NzPzRmYTgiLCJ3ZWJwYWNrOi8vLy4vd2ViL2Fzc2V0cy9qcy9hcHAuanMiXSwibmFtZXMiOlsicmVxdWlyZSIsImNvbnNvbGUiLCJsb2ciXSwibWFwcGluZ3MiOiI7Ozs7Ozs7OztBQUFBLHVDOzs7Ozs7Ozs7OztBQ0FBO0FBRUFBLG1CQUFPLENBQUMsZ0RBQUQsQ0FBUDs7QUFFQUMsT0FBTyxDQUFDQyxHQUFSLENBQVksc0JBQVosRSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyIvLyBleHRyYWN0ZWQgYnkgbWluaS1jc3MtZXh0cmFjdC1wbHVnaW4iLCIvLyBwdWJsaWMvYXNzZXRzL2pzL2FwcC5qc1xyXG5cclxucmVxdWlyZSgnLi4vY3NzL2FwcC5jc3MnKTtcclxuXHJcbmNvbnNvbGUubG9nKCdIZWxsbyBXZWJwYWNrIEVuY29yZScpOyJdLCJzb3VyY2VSb290IjoiIn0=

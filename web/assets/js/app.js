a1e31b77a608a031567b7351659060b3dcbe8ac6
poulain35 <poulain.jeremy1@gmail.com>
2018-12-07 07:57:07 +0100

    front mdb

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,5 +1,10 @@
 // public/assets/js/app.js
 
 require('../css/app.css');
-require('../../../node_modules/mdbootstrap/css/mdb.css');
+
+require('../../../node_modules/mdbootstrap/css/bootstrap.css');
 require('../../../node_modules/mdbootstrap/css/mdb.min.css');
+
+require('../../../node_modules/mdbootstrap/js/jquery-3.3.1.min.js');
+require('../../../node_modules/mdbootstrap/js/bootstrap.min.js');
+require('../../../node_modules/mdbootstrap/js/mdb.min.js');
